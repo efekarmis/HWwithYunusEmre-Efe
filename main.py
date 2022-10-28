151e157b209e599ba3870fd86542e5cf59f54dd0
DolphyWind <yunusaydin590@gmail.com>
2022-10-28 19:40:30 +0300

    get Position

--- main.py
+++ main.py
@@ -1,23 +1,11 @@
 from pymavlink import mavutil 
 
 address = 'udpin:localhost:14551' #simulasyon
-# address= '/dev/ttyACM0' #pixhawk usb
-# address= '/dev/ttyTHS1' #pixhawk telem2 baudrate= 115200
 vehicle= mavutil.mavlink_connection(address,baudrate=57600,autoreconnect= True)
 vehicle.wait_heartbeat()
 print("baglanti basarili")
 
-# while True:
-#     message= vehicle.recv_match(blocking= True)
-#     print(message)
-
-
-battery= vehicle.recv_match(type='BATTERY_STATUS', blocking=True)  
-print(f"Pil Yuzdesi: {battery.battery_remaining}")
+def getPosition():
+    message = vehicle.recv_match(type='GLOBAL_POSITION_INT', blocking= True)
+    alt = message.relative_alt / 1000
     
-message= vehicle.recv_match(type='VFR_HUD', blocking=True)
-print(f"Airspeed:  {message.airspeed}")
-
-msg = vehicle.recv_match(type = 'HEARTBEAT', blocking = True)
-mode = mavutil.mode_string_v10(msg)
-print(f"Mode: {mode}")
